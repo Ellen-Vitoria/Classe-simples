2724a26109751173b063a5e0d48438dd3cc85932
ellen <ellen.vitoria.ogs@gmail.com>
2020-05-12 20:55:50 -0300

    Exemplo ao exibir Método com Parâmetro

--- index.php
+++ index.php
@@ -11,4 +11,13 @@ $produto->qt = "2";
 $produto->taxa = "0.50";
 
 echo "Nome do Produto: {$produto->nome}<p>"; //Exibindo Valor que foi inserido ao Atributo $nome
-$produto->calcularFrete(); //Como está exibindo um Método, o "echo" deve estar dentro desse Método para aparecer a Frase e o Cálculo
+$produto->calcularFrete(); //Como está exibindo um Método Sem Parâmetro, o "echo" deve estar dentro desse Método para aparecer a Frase e o Cálculo
+
+//Exibir Método com Parâmetro:
+/*
+$produto = new Produto();
+$produto->nome = "Fone";
+$produto->descricao = "Aparelho de escuta";
+
+$produto->calcularFrete(50, 2, 0.50);
+*/
