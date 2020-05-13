10d722d095ff156e17eab093845fdd7a4e0ea5da
ellen <ellen.vitoria.ogs@gmail.com>
2020-05-12 21:50:24 -0300

    Alterações mínimas

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-//Require - Verificar se a Classe existe
+//Require - Verificar se a Classe Produto existe
 require __DIR__ . './produto.php';   //__DIR__: Variável Global
 
 $produto = new Produto();
@@ -11,6 +11,7 @@ $produto->qt = "2";
 $produto->taxa = "0.50";
 
 echo "Nome do Produto: {$produto->nome}<p>"; //Exibindo Valor que foi inserido ao Atributo $nome
+//echo $produto->calcularFrete() ou $produto->calcularFrete(); -- Mesmo Resultado
 $produto->calcularFrete(); //Como está exibindo um Método Sem Parâmetro, o "echo" deve estar dentro desse Método para aparecer a Frase e o Cálculo
 
 //Exibir Método com Parâmetro:
@@ -19,5 +20,5 @@ $produto = new Produto();
 $produto->nome = "Fone";
 $produto->descricao = "Aparelho de escuta";
 
-$produto->calcularFrete(50, 2, 0.50);
+echo $produto->calcularFrete(50, 2, 0.50) ou $produto->calcularFrete(50, 2, 0.50);
 */
