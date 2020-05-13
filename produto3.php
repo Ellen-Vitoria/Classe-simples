e5f2b5dff1e839df4da60617a0d0e32c6bda8d62
ellen <ellen.vitoria.ogs@gmail.com>
2020-05-13 09:32:33 -0300

    Inserindo Classe Produto3

---
+++ produto3.php
@@ -0,0 +1,46 @@
+<?php
+
+Class Produto3
+{
+    public $nome, $descricao, $preco, $qt, $taxa, $frete;
+
+    public function getPreco()
+    {
+        return $this->preco;
+    }
+
+    public function setPreco($preco)
+    {
+        $this->preco = $preco;
+    }
+
+    public function getQt()
+    {
+        return $this->qt;
+    }
+
+    public function setQt($qt)
+    {
+        $this->qt = $qt;
+    }
+
+    public function getTaxa()
+    {
+        return $this->taxa;
+    }
+
+    public function setTaxa($tx)
+    {
+        $this->taxa = $tx;
+    }
+
+    public function getFrete()
+    {
+        return $this->frete;
+    }
+
+    public function setFrete($frete)
+    {
+        $this->frete = $frete;
+    }
+}
