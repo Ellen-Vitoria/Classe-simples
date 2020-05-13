0de2830413fa3b56c4984ffc19db88439f43ae67
ellen <ellen.vitoria.ogs@gmail.com>
2020-05-13 13:23:14 -0300

    Alterando forma de exibir Classe Produto3

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 
-//echo '<pre>'; -- Deixar o código bem definido/formatado, fica melhor no browser
-
 /*Require - Verificar se a Classe Produto existe
   __DIR__: Variável Global
 
@@ -29,7 +27,7 @@ $produto->calcularFrete(); // Ou echo $produto->calcularFrete(); // echo "{$prod
 
 //Classe Produto2 (com Parêmetro no Método):
 $produto = new Produto2('Fone', 'Aparelho de escuta', 50, 2, 0.5); // Usando "_construct" é obrigatório inserir todos os valores direto na Instância
-echo "<p>O Frete do {$produto->nome} é: R$ {$produto->calcularFrete()}"; // É recomendado colocar essa linha no Método, assim feito como na Classe anterior
+echo "<p>O Frete do {$produto->nome} é: R$ {$produto->calcularFrete()}<p>"; // É recomendado colocar essa linha no Método, assim feito como na Classe anterior
 
 /* Sem o Método _construct, ficaria da seguinte forma
      $produto = new Produto();
@@ -61,7 +59,9 @@ $produto->descricao = "Aparelho de escuta";
 $produto->setPreco(50); //No caso de texto, colocar aspas simples
 $produto->setQt(2);
 $produto->setTaxa(0.5);
+$produto->frete = $produto->calcularFrete();
 
-var_dump($produto);
+//print_r($produto);
+echo "<pre>", print_r($produto), "</pre>"; //echo '<pre>'; -- Deixar o código bem definido/formatado, fica melhor no browser
+
 echo "O Frete do {$produto->nome} é = R$ {$produto->calcularFrete()}";
-//echo '<pre>';
